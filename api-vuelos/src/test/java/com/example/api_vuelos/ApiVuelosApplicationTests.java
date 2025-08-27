ddc326f1f9a23d0f7c0402b588dd5a2ba6eb5c5e
lupeMartinezJulian <guadalupemartinezjulian@gmail.com>
2025-08-27 21:45:34 +0200

    Subida base Spring

---
+++ api-vuelos/src/test/java/com/example/api_vuelos/ApiVuelosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_vuelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVuelosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
