c57a5d0cdbb76ffad577837ba3f4621ac340d411
lupeMartinezJulian <guadalupemartinezjulian@gmail.com>
2025-09-01 10:25:16 +0200

    Documentacion

--- api-vuelos/src/main/java/com/example/api_vuelos/ApiVuelosApplication.java
+++ api-vuelos/src/main/java/com/example/api_vuelos/ApiVuelosApplication.java
@@ -1,13 +1,24 @@
 package com.example.api_vuelos;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiVuelosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiVuelosApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("API vuelos")
+				.version("1.0")
+				.description("API para filtrar, buscar, editar, agregar y eliminar vuelos"));
 	}
 
 }
